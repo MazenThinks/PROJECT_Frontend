1f44c4245b019ee3e4d8c15c372575cd6227105f
Mazen Magdy <mazenmagdy190.mm@gmail.com>
2025-02-20 17:04:45 +0200

    Formatting Documents

--- script.js
+++ script.js
@@ -2,8 +2,7 @@
 var countDownDate = new Date("oct 5, 2025 0:0:0").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
-
+var x = setInterval(function () {
   // Get today's date and time
   var now = new Date().getTime();
 
@@ -17,8 +16,8 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
